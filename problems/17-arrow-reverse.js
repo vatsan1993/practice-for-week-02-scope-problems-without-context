d76c7c22fe5759a532fd250b809d493f5c034d8e
Srivatsan <vatsan1993@gmail.com>
2023-05-04 00:09:44 +0530

    finished 18th program

--- problems/17-arrow-reverse.js
+++ problems/17-arrow-reverse.js
@@ -1,18 +1,19 @@
 /***********************************************************************
 Write a function reverseStr(str) that accepts a string and returns that string
-reversed. 
-
+reversed.
 Write this function using a fat arrow function!
-
 Examples:
 let result1 = reverseStr("hello"); // returns "olleh"
 let result2 = reverseStr("garden"); // returns "nedrag"
 let result3 = reverseStr("potato"); // returns "otatop"
-
-
 ***********************************************************************/
-
-// Your code here
+let reverseStr = (str) => {
+  let reversedString = '';
+  for( let i = str.length - 1; i >= 0; i-- ){
+    reversedString += str[i];
+  }
+  return reversedString;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
