37bb84d5f8984182794e22dae730933640a1bad6
Srivatsan <vatsan1993@gmail.com>
2023-05-03 23:47:10 +0530

    finished 15th program

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -1,16 +1,12 @@
 /***********************************************************************
 Write a function named `smoothieMachine` that accepts any number of parameters
 and returns a function.
-
 The returned function will also accept any number of parameters and will
 return a string including all of the parameters of smoothieMachine as well
-as the returned function's parameters. Look at the examples for a guide of 
+as the returned function's parameters. Look at the examples for a guide of
 how your return should be formatted!
-
 See below for examples:
-
 let smoothie1 = smoothieMachine();
-
 console.log(smoothie1("milk"));
 // prints "I'm having a smoothie with milk"
 console.log(smoothie1("kale", "spinach"));
@@ -25,7 +21,24 @@ console.log(smoothie2("pineapple"));
 
 const smoothieMachine = (...ingredients) => {
   // Your code here
+  let allIngredients = [...ingredients];
+  return (...ingredients2) => {
+    let message = "I'm having a smoothie with ";
+    for(let ingredient of ingredients2){
+      allIngredients.push(ingredient);
+    }
+    return message + allIngredients.join(" and ");
+  }
 };
+
+// let smoothie1 = smoothieMachine();
+// console.log(smoothie1("milk"));
+// // prints "I'm having a smoothie with milk"
+// console.log(smoothie1("kale", "spinach"));
+// // prints "I'm having a smoothie with milk and kale and spinach"
+// console.log(smoothie1("honey", "pears", "berries"));
+// // prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
