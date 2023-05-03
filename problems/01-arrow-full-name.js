d22087a106f808e75f70d9ee19ceee02dd4e97c5
Srivatsan <vatsan1993@gmail.com>
2023-05-03 18:53:56 +0530

    finished 2nd program

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a function using fat arrow syntax, `arrowGetFullName(person)` that takes in
-a person object and returns a string containing their full name. 
+a person object and returns a string containing their full name.
 
 Assign the below function to a variable using the const keyword. Using the const
 keyword will allow any value assigned to that variable protection from being
@@ -15,7 +15,7 @@ arrowGetFullName(p2); // => 'Charlie Brown'
 ***********************************************************************/
 
 // Your code here
-
+let arrowGetFullName = (person) => `${person.firstName} ${person.lastName}`
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = arrowGetFullName;
