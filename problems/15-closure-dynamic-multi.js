d32ac7806d703759c7a62693ea0dbd534ad8e36b
Srivatsan <vatsan1993@gmail.com>
2023-05-03 23:59:15 +0530

    finished 16th program

--- problems/15-closure-dynamic-multi.js
+++ problems/15-closure-dynamic-multi.js
@@ -2,24 +2,16 @@
 Let's practice writing closures by creating a method called `dynamicMultiply(num)`.
 The dynamicMultiply function will return a new function that will allow us to
 create new separate custom multiply functions.
-
-
 Look below to see how this function is invoked:
-
 const doubler = dynamicMultiply(2); // returns a functions
 doubler(5); // returns 10
-
 const tripler = dynamicMultiply(3);
 tripler(5); // returns 15
-
 const multiplyByFive = dynamicMultiply(5);
 multiplyByFive(5); // returns 25
-
-
 ***********************************************************************/
-
 function dynamicMultiply(num) {
-  // Your code here
+  return (val) => num * val;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
