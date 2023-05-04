cb3b4a521bc675a77f8a82e3b8889fbc7e21ca17
Srivatsan <vatsan1993@gmail.com>
2023-05-04 07:34:41 +0530

    finished all programs

--- problems/20-curried-sum.js
+++ problems/20-curried-sum.js
@@ -1,14 +1,11 @@
 /***********************************************************************
-
 Currying is the process of decomposing a function that takes multiple arguments
 into one that takes single arguments successively until it has the sufficient
 number of arguments to run.This technique is named after the
 logician Haskell Curry(the functional programming language Haskell is, too).
-
 Write a `curriedSum` function that takes an integer(how many numbers to sum)
 and returns a function that can be successively called with single arguments
 until it finally returns a sum.
-
 Here is a breakdown of how curriedSum(numArgs) should work:
     - Define an empty array, `numbers`.
     - Define a function, `_curriedSum` that:
@@ -19,12 +16,9 @@ Here is a breakdown of how curriedSum(numArgs) should work:
         returns the result.
         - Else, it returns itself.
     - Returns `_curriedSum`.
-
 If you're confused, think of it this way: `_curriedSum` keeps collecting
 arguments and returning itself until it has enough arguments, at which point it
 actually does the required work of summing.
-
-
 Example:
 // 1
 const sum = curriedSum(4); // returns a function
@@ -32,19 +26,33 @@ sum(5) // returns a function
 sum(20) // returns a function
 sum(30) // returns a function
 sum(20); // => returns 75
-
 // 2
 // this function can also be invoked like this:
 const sum = curriedSum(3)(2)(1)(7); // => returns 10
-
 AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 - Come up with at least two situations (one per person) on when currying would
   be useful
 ***********************************************************************/
-
 function curriedSum(numArgs) {
   // Your code here
+  let numbers = [];
+  let total = 0;
+  if(numbers.length >= numArgs){
+    return total
+  }
+  let _curriedSum = (num) =>{
+    numbers.push(num)
+    total += num;
+    if(numbers.length >= numArgs){
+      return total
+    }else{
+      return _curriedSum;
+    }
+  }
+  return _curriedSum
 }
+
+const sum = curriedSum(3)(2)(1)(7);
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = curriedSum;
